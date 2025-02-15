286c46cd6c3fd25c80256cd19702ebcad9f5e835
GersonPSJunior <gersonjunior1771@gmail.com>
2025-02-15 14:44:53 -0300

    Initial commit of the project

---
+++ anime-service/src/test/java/br/com/duosdevelop/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duosdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
